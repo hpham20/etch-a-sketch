9d094bf6c02da43d35557e01d83a6dad1879a465
henry <henrypham20@gmail.com>
2023-09-09 11:54:50 -0500

    Implemented clear and random color function

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const grid = document.querySelector(".grid");
 let gridSize = prompt("How many squares per side? (Maximum: 100)");
-let color = "black"
+let colorMode = 1
 
 createGrid(gridSize);
 
 function createSquare(size) {
-    const div = document.createElement("div");
+    const square = document.createElement("div");
 
-    div.classList.add("box");
-    div.style.width = `${size}px`;
-    div.style.height = `${size}px`;
-    return div;
+    square.classList.add("box");
+    square.style.width = `${size}px`;
+    square.style.height = `${size}px`;
+
+    return square;
 }
 
 function createGrid(gridSize) {
@@ -18,27 +19,45 @@ function createGrid(gridSize) {
 
     const columnWidth = `${100 / gridSize}%`
 
-    grid.style.gridTemplateColumns = `repeat(${gridSize}, ${columnWidth})`;
-    grid.style.gridTemplateRows = `repeat(${gridSize}, ${columnWidth})`;
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, ${columnWidth})`
+    grid.style.gridTemplateRows = `repeat(${gridSize}, ${columnWidth})`
     
+    grid.innerHTML = ""
+
     for (let i = 0; i < gridSize * gridSize; i++) {
-            grid.appendChild(createSquare(grid.clientWidth / gridSize));
+        grid.appendChild(createSquare(grid.clientWidth / gridSize));
     }
 }
 
 grid.addEventListener("mouseover", function (e) {
-    if (e.target.matches(".box")) {
-        e.target.classList.add("active");
+    if (e.target.classList.contains("box")) {
+        if (colorMode == 1) {
+            e.target.style.backgroundColor = "black"
+        } else {
+            let letters = '0123456789ABCDEF';
+            let randomColor = '#';
+            for (let i = 0; i < 6; i++) {
+                randomColor += letters[Math.floor(Math.random() * 16)];
+            }
+            e.target.style.backgroundColor = randomColor;
+        }
     }
 });
 
 function clearCanvas() {
     let squares = document.getElementsByClassName("box")
     for (let i = 0; i < squares.length; i++) {
-        squares[i].classList.remove("active")
+        squares[i].style.backgroundColor = "transparent"
     }
 }
 
+function setDefaultColor() {
+    colorMode = 1
+}
+
+function setRandomColor() {
+    colorMode = 2
+}
 /*
 TODO:
 - Slider to adjust canvas size
